fc81ece4cd6370ffd49be7d120bce435a4519381
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2024-12-31 11:02:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-//your JS code here. If required.
-function isLeapYear(year) {
-  // A year is a leap year if it is divisible by 4, 
-  // but not divisible by 100 unless also divisible by 400
-  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
-    return true;
-  }
-  return false;
-
 function daysOfAYear(year) {
-	if(year==isLeapYear(year)){
-		return "366";
-	}
-	else{
-		return "365"
-	}
+  // A year is a leap year if it is divisible by 4, but not by 100 unless divisible by 400
+  if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0)) {
+    return 366; // Leap year
+  }
+  return 365; // Non-leap year
 }
